9916d0fd880664c50a07bc43befed081cc191081
HadleysHope <sammyjohn123@protonmail.com>
2023-06-30 14:37:45 +1200

    Added Get all Products to Inventory

--- client/src/components/content/inventory/Inventory.js
+++ client/src/components/content/inventory/Inventory.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Inventory.css";
+import axios from "axios";
 
 const Inventory = () => {
+  const[products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/products');
+        console.log(response.data);
+        setProducts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (products.length === 0) {
+    return <div>Loading...</div>;
+  }
+  
+
   return (
     <section className="dashboard-content">
       <div className="stock-content">
@@ -18,76 +40,79 @@ const Inventory = () => {
           <p className="placeholder">$50,000</p>
         </div>
       </div>
-
-      <div className="stock-table">
-        <h4 className="category-title">Solar Panels</h4>
-        <table>
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Total Value</th>
-              <th>Sold</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Product 1</td>
-              <td>10</td>
-              <td>$100</td>
-              <td>$1,000</td>
-              <td>Yes</td>
-              <td>Action 1</td>
-            </tr>
-            <tr>
-              <td>Product 2</td>
-              <td>5</td>
-              <td>$200</td>
-              <td>$1,000</td>
-              <td>No</td>
-              <td>Action 2</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <div className="stock-table">
-        <h4 className="category-title">Batteries</h4>
-        <table>
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Total Value</th>
-              <th>Sold</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Product 3</td>
-              <td>8</td>
-              <td>$150</td>
-              <td>$1,200</td>
-              <td>Yes</td>
-              <td>Action 3</td>
-            </tr>
-            <tr>
-              <td>Product 4</td>
-              <td>3</td>
-              <td>$300</td>
-              <td>$900</td>
-              <td>No</td>
-              <td>Action 4</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="product-list">
+        <hr />
+        <div className="table">
+          <div className="--flex-between --flex-dir-column">
+            <span>
+              <h3>Inventory Items</h3>
+            </span>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Dimensions</th>
+                <th>Wattage</th>
+                <th>Efficiency</th>
+                <th>Warranty</th>
+                <th>Weight</th>
+                <th>Price</th>
+                <th>Current Stock</th>
+                <th>Minimum Stock</th>
+                <th>Serial Number</th>
+                <th>Category</th>
+                <th>Value</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product.name}</td>
+                  <td>{product.dimensions}</td>
+                  <td>{product.wattage}</td>
+                  <td>{product.efficiencyPercentage}</td>
+                  <td>{product.warranty}</td>
+                  <td>{product.weight}</td>
+                  <td>{product.price}</td>
+                  <td>{product.currentStock}</td>
+                  <td>{product.minimumStock}</td>
+                  <td>{product.serialNumber}</td>
+                  <td>{product.category_id}</td>
+                  <td>{product.price * product.currentStock}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </section>
   );
 };
 
 export default Inventory;
+
+/*
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const Inventory = () => {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/products');
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return null;
+};
+
+export default Inventory;
+*/
